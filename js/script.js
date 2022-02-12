2c050a213590d266ab92e5817dd4fa70caeb71ee
Auriel Harlih <arilharlih25@gmail.com>
2022-02-12 18:55:47 +0700

    add responsiveness

--- js/script.js
+++ js/script.js
@@ -1,8 +1,23 @@
 const menuIcon = document.querySelector(".menu-icon");
+const navbar = document.getElementById("navbar");
+const offset = navbar.offsetTop;
 
 menuIcon.addEventListener("click", () => {
   menuIcon.classList.toggle("active");
   for (let i = 0; i < menuIcon.children.length; i++) {
     menuIcon.children[i].classList.remove("no-animation");
   }
+  navbar.classList.toggle("active");
 });
+
+const checkNavbarTop = () => {
+  if (window.scrollY > offset) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+};
+
+window.onscroll = function () {
+  checkNavbarTop();
+};
